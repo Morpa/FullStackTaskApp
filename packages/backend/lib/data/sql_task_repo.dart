7b1c0ef4b915833f84ba9155164723939d42cc63
morpa <andremorpa@gmail.com>
2023-08-10 11:04:20 +0100

    Implementing CRUD

---
+++ packages/backend/lib/data/sql_task_repo.dart
@@ -0,0 +1,33 @@
+import 'package:shared/shared.dart';
+
+class SQLTaskRepo implements TaskRepo {
+  @override
+  Future<Task> addTask(Task task) {
+    // TODO: implement addTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteTask(String id) {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>> fetchAllTasks() {
+    // TODO: implement fetchAllTasks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Task> getTaskById(String id) {
+    // TODO: implement getTaskById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateTask(Task task) {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
+}
