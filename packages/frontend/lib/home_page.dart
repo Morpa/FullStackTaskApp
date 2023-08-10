5f1fe85bb2aa4c4d0801c6dac726ac896fedffdf
morpa <andremorpa@gmail.com>
2023-08-10 19:54:12 +0100

    Main page UI

--- packages/frontend/lib/home_page.dart
+++ packages/frontend/lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared/shared.dart';
+
+import 'widgets/task_list_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,12 +12,45 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tasks'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(),
-          )
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: TaskListView(
+                  tasks: [
+                    Task(id: 'id', title: 'title', isCompleted: false),
+                    Task(id: 'id2', title: 'title2', isCompleted: true),
+                    Task(id: 'id3', title: 'title3', isCompleted: false),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'New task',
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.done),
+            )
+          ],
+        ),
       ),
     );
   }
